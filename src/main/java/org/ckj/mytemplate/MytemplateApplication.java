7c9f50e36e5c8a240d7fa0854db7134fe53d052d
admin <1660987689@qq.com>
2024-11-07 17:22:14 +0800

    feat:create program

---
+++ src/main/java/org/ckj/mytemplate/MytemplateApplication.java
@@ -0,0 +1,13 @@
+package org.ckj.mytemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ckj/mytemplate/MytemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ckj.mytemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
